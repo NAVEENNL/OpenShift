400e7cd979f35518142f7eb3a234b92a0104aa00
NAVEENNL <nkumar@rocketsoftware.com>
2026-07-07 03:12:28 -0400

    This is Initial Test Code

---
+++ ConsoleDb2DotNet8App/Program.cs
@@ -0,0 +1,22 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+using IBM.Data.Db2;
+
+Console.WriteLine("Using DB2 .NET8 provider");
+
+
+//Connection String
+
+string connString = Environment.GetEnvironmentVariable("ConnectionString");
+
+DB2Connection con = new DB2Connection(connString);
+
+con.Open();
+
+Console.WriteLine("Connection Opened successfully");
+Console.WriteLine("Database Name: "+ con.Database);
+
+con.Close();
+
+Console.WriteLine("Connection Closed");
